7d8d3f792a497fe873295c4221dc4a09600f10c4
«Fabienne <fabiennedgrv@gmail.com>
2025-06-08 19:38:20 +0200

    allowed hosts

--- django_docker_demo/settings.py
+++ django_docker_demo/settings.py
@@ -18,10 +18,11 @@ assert SECRET_KEY, "La variable SECRET_KEY doit être définie !"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = os.getenv('DEBUG') == 'False'
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1", "djangodocker-production.up.railway.app").split(",")
+#ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "djangodocker-production.up.railway.app").split(",")
 
 CSRF_TRUSTED_ORIGINS = [
     f"https://{host}"
